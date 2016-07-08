d31b95142d4cda26ceffd45258be3dce4e972b45
Louis <luqingfei@126.com>
2016-07-08 15:51:22 +0800

    提交

---
+++ xamarin-forms-samples/XAMLSamples/XamlSamples/XamlSamples/AbsoluteLayoutPage.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XamlSamples
+{
+    /// <summary>
+    /// 绝对布局
+    /// </summary>
+    public partial class AbsoluteLayoutPage
+    {
+        public AbsoluteLayoutPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
